47227ea3b4c0c59033425fa93541d132e67eb969
laxmanRao <laxmanraoganapathi0306@gmail.com>
2025-06-25 15:12:59 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,17 @@
+document.title = "Java DOM Example";
+
+document.getElementById("header").innerText = "Hello, Java DOM!";
+document.getElementById("header").style.color = "blue";
+document.getElementById("header").style.fontSize = "50px";
+
+document.getElementById("mr").style.color="red";
+document.getElementById("mr").style.fontSize="100px";
+document.getElementsByClassName("hr")[0].style.color = "red";
+document.getElementsByClassName("hr")[1].style.color = "red";
+document.getElementsByClassName("hr")[2].style.color = "red";
+document.getElementsByClassName("hr")[3].style.color= "red";
+document.getElementsByClassName("hr")[0].style.fontSize= "100px";
+document.getElementsByClassName("hr")[1].style.fontSize= "100px";
+document.getElementsByClassName("hr")[2].style.fontSize= "100px";
+document.getElementsByClassName("hr")[3].style.fontSize= "100px";
+document.body.style.backgroundColor = "black";
