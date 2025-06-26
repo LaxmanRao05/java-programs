34d4d95dd89f27af2b51cc8daf6487f47ee2c1a4
laxmanRao <laxmanraoganapathi0306@gmail.com>
2025-06-26 15:27:23 +0530

    Add files via upload

---
+++ dom1.js
@@ -0,0 +1,40 @@
+document.title="java.js"
+document.getElementById("mrcet").innerHTML="hello,world";
+document.getElementById("mrcet").style.color="green";
+document.getElementById("mr").style.color="red";
+document.getElementById("mr").style.fontSize ="30px";
+//  let naveen=document.getElementsByClassName("id");
+//  naveen[0].style.color="red";
+//  naveen[1].style.color="white";
+//  naveen[2].style.color="green";
+//  naveen[3].style.color="yellow";
+// //  naveen[0].style.fontSize="100p"
+//  naveen[1].style.fontSize="100px"
+//  naveen[2].style.fontSize="100px"
+//  naveen[3].style.fontSize="100px"
+
+let kittu=document.querySelectorAll(".EB");
+kittu [0].style.color="red";
+kittu [0].style.fontSize="30px";
+kittu [1].style.color="green";
+kittu [1].style.fontSize="30px";
+
+function changeBackground(){
+    if(  document.body.style.backgroundColor === "blue")
+    {  document.body.style.backgroundColor = "red"}
+    else{  document.body.style.backgroundColor = "blue";}
+  }
+  function changetext(){
+    document.getElementById("ktm").innerHTML ="ktm125,ktm1390";
+  }
+  
+
+// document.getElementsByClassName("id")[0].style.color="red";
+// document.getElementsByClassName("id")[1].style.color="green";
+// document.getElementsByClassName("id")[2].style.color="bule";
+// document.getElementsByClassName("id")[3].style.color="yellow";
+// document.getElementsByClassName("id")[0].style.fontSize="200px";
+// document.getElementsByClassName("id")[1].style.fontSize="250px";
+// document.getElementsByClassName("id")[2].style.fontSize="300px";
+// document.getElementsByClassName("id")[3].style.fontSize="250px";
+ document.body.style.backgroundColor="pink";
